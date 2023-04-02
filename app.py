f43130f78a06261626b777183c72ccfff4ab85eb
caroljoyv <caroljoyv27@gmail.com>
2023-04-02 15:11:46 +0530

    fixed typo

--- app.py
+++ app.py
@@ -7,6 +7,6 @@ st.set_page_config(page_title = "My Webpage", page_icon = ":tada:",layout = "wid
 #--------Header Section-----------#
 st.subheader("Hi, I am Carolene :wave:")
 st.title("A Machine Leaning enthusiast ")
-st.write("I am passionate about solving solutions for complex problems using machine leaninig algorithms.")
+st.write("I am passionate about solving solutions for complex problems using machine learning algorithms.")
 
 
