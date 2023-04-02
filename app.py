1829208e7450cb41a3e3629b82347b74daaa3ad2
caroljoyv <caroljoyv27@gmail.com>
2023-04-02 15:05:54 +0530

    initial commit created app.py and requirements.txt

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+
+st.set_page_config(page_title = "My Webpage", page_icon = ":tada:",layout = "wide")
+
+
+#--------Header Section-----------#
+st.subheader("Hi, I am Carolene :wave:")
+st.title("A Machine Leaning enthusiast ")
+st.write("I am passionate about solving solutions for complex problems using machine leaninig algorithms.")
+
+
